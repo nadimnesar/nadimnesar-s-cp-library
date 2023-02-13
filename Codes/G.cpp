df7ed8b3ee384cd6d08550712a7007824399fc8c
Nesar Ahmed <nadimahmednesar@gmail.com>
2023-02-14 01:25:26 +0600

    up

--- Codes/G.cpp
+++ Codes/G.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #define log(b, x)           (double)(log(x)/log(b))
 
 const ll mod = 1e9 + 7;
-const ll maxx = 3e4 + 7;
+const ll maxx = 1e6 + 7;
 const ll inf = LLONG_MAX;
 const double eps = 1e-7; 
 const double pi = acos(-1.0);
--- String/Hashing/backwardhash2D.cpp
+++ String/Hashing/backwardhash2D.cpp
@@ -19,9 +19,8 @@ const ll maxx = 1e5 + 7;
 ll base[2] = { 169, 369 };
 ll mod[2] = { 1000003769, 1000004569 };
 ll basePow[2][maxx];
-
-ll hashTable[2][maxx];
-ll hashTable2[2][maxx];
+ll front_hashTable[2][maxx];
+ll back_hashTable[2][maxx];
 ll n;
 
 void init() {
@@ -32,35 +31,35 @@ void init() {
     }
 }
 
-void build_hashTable(string &str) { // 1 based idx
-    hashTable[0][1] = hashTable[1][1] = (str[0] - 'a' + 1);
+void build_front_hashTable(string &str) { // 1 based idx
+    front_hashTable[0][1] = front_hashTable[1][1] = (str[0] - 'a' + 1);
     for (ll i = 1; str[i]; i++) {
-        hashTable[0][i + 1] = ((hashTable[0][i] * base[0]) + (str[i] - 'a' + 1)) % mod[0];
-        hashTable[1][i + 1] = ((hashTable[1][i] * base[1]) + (str[i] - 'a' + 1)) % mod[1];
+        front_hashTable[0][i + 1] = ((front_hashTable[0][i] * base[0]) + (str[i] - 'a' + 1)) % mod[0];
+        front_hashTable[1][i + 1] = ((front_hashTable[1][i] * base[1]) + (str[i] - 'a' + 1)) % mod[1];
     }
 }
 
 pair < ll, ll > get_subHash(ll left, ll right) { // 1 based idx
     pair < ll, ll > sub;
-    sub.first = (hashTable[0][right] - (hashTable[0][left - 1] * basePow[0][right - left + 1])) % mod[0];
-    sub.second = (hashTable[1][right] - (hashTable[1][left - 1] * basePow[1][right - left + 1])) % mod[1];
+    sub.first = (front_hashTable[0][right] - (front_hashTable[0][left - 1] * basePow[0][right - left + 1])) % mod[0];
+    sub.second = (front_hashTable[1][right] - (front_hashTable[1][left - 1] * basePow[1][right - left + 1])) % mod[1];
     if (sub.first < 0) sub.first += mod[0];
     if (sub.second < 0) sub.second += mod[1];
     return sub;
 }
 
-void build_backward_hashTable(string &str) { // 1 based idx
-    hashTable2[0][n] = hashTable2[1][n] = (str[n - 1] - 'a' + 1);
+void build_back_hashTable(string &str) { // 1 based idx
+    back_hashTable[0][n] = back_hashTable[1][n] = (str[n - 1] - 'a' + 1);
     for (ll i = n - 2; i >= 0; i--) {
-        hashTable2[0][i + 1] = ((hashTable2[0][i + 2] * base[0]) + (str[i] - 'a' + 1)) % mod[0];
-        hashTable2[1][i + 1] = ((hashTable2[1][i + 2] * base[1]) + (str[i] - 'a' + 1)) % mod[1];
+        back_hashTable[0][i + 1] = ((back_hashTable[0][i + 2] * base[0]) + (str[i] - 'a' + 1)) % mod[0];
+        back_hashTable[1][i + 1] = ((back_hashTable[1][i + 2] * base[1]) + (str[i] - 'a' + 1)) % mod[1];
     }
 }
 
 pair < ll, ll > get_backward_subHash(ll left, ll right) { // 1 based idx
     pair < ll, ll > sub;
-    sub.first = (hashTable2[0][left] - (hashTable2[0][right + 1] * basePow[0][right - left + 1])) % mod[0];
-    sub.second = (hashTable2[1][left] - (hashTable2[1][right + 1] * basePow[1][right - left + 1])) % mod[1];
+    sub.first = (back_hashTable[0][left] - (back_hashTable[0][right + 1] * basePow[0][right - left + 1])) % mod[0];
+    sub.second = (back_hashTable[1][left] - (back_hashTable[1][right + 1] * basePow[1][right - left + 1])) % mod[1];
     if (sub.first < 0) sub.first += mod[0];
     if (sub.second < 0) sub.second += mod[1];
     return sub;
@@ -81,8 +80,8 @@ void solve() {
     string s;
     cin >> s;
 
-    build_hashTable(s);
-    build_backward_hashTable(s);
+    build_front_hashTable(s);
+    build_back_hashTable(s);
 
     ll l = 1;
     ll r = n;
@@ -110,7 +109,14 @@ int main() {
 
     init();
 
-    solve();
+    ll t;
+    cin >> t;
+    while(t--){
+        solve();
+        memset(front_hashTable, 0);
+        memset(back_hashTable, 0);
+    }
+    
 
     return 0;
 }
