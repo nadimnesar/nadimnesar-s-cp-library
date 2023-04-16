d45a8c3567e911a20a6a68e486c7ebf5e45cc797
Nesar Ahmed <neasr15-13636@diu.edu.bd>
2023-04-16 14:56:30 +0600

    update

--- Codes/F.cpp
+++ Codes/F.cpp
@@ -16,7 +16,7 @@ using namespace std;
 const ll mod = 1e9 + 7;
 const ll maxx = 1e6 + 7;
 const ll inf = LLONG_MAX;
-const double eps = 1e-7; 
+const double eps = 1e-7;
 const double pi = acos(-1.0);
 
 void solve() {
