ddebf0f1ad2a3f42e0170def26d7e6f7a7c93612
Nesar Ahmed <neasr15-13636@diu.edu.bd>
2023-04-29 14:04:51 +0600

    TAB 4 TO 2

--- Codes/F.cpp
+++ Codes/F.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll                  long long int
-#define endl                '\n'
-#define in                  insert
-#define pb                  push_back
+#define ll              long long int
+#define endl            '\n'
+#define in              insert
+#define pb              push_back
 
-#define full(x)             x.begin(),x.end()
-#define dot(x)              fixed<<setprecision(x)
+#define full(x)         x.begin(),x.end()
+#define dot(x)          fixed<<setprecision(x)
 
-#define gcd(x, y)           (ll)(__gcd(x, y))
-#define lcm(x, y)           (ll)((x/gcd(x,y))*y)
-#define log(b, x)           (double)(log(x)/log(b))
+#define gcd(x, y)       (ll)(__gcd(x, y))
+#define lcm(x, y)       (ll)((x/gcd(x,y))*y)
+#define log(b, x)       (double)(log(x)/log(b))
 
 const ll mod = 1e9 + 7;
 const ll maxx = 1e6 + 7;
@@ -20,15 +20,15 @@ const double eps = 1e-7;
 const double pi = acos(-1.0);
 
 void solve() {
-
+  
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
+  ios::sync_with_stdio(false); cin.tie(nullptr);
 
-    ll t;
-    cin >> t;
-    while (t--) solve();
+  ll t;
+  cin >> t;
+  while (t--) solve();
 
-    return 0;
+  return 0;
 }
