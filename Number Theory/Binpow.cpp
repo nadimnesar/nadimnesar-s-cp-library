ff44024de1ab46ebe9e43eae5820af30870c1725
Nesar Ahmed <neasr15-13636@diu.edu.bd>
2023-09-13 12:04:31 +0600

    up

--- Number Theory/Binpow.cpp
+++ Number Theory/Binpow.cpp
@@ -1,6 +1,6 @@
 ll binpow(ll n, ll p){
     if(p == 0) return 1;
     ll res = binpow(n, p/2);
-    if(p%2) return res*res*n;
+    if(p&1) return res*res*n;
     else return res*res;
 }
--- Number Theory/Sieve.cpp
+++ Number Theory/Sieve.cpp
@@ -7,26 +7,26 @@ using namespace std;
 vector < ll > primes;
 vector < bool > ifprime(maxx);
 
-void Sieve(){
-    for(ll i = 3; i*i <= maxx; i += 2){
-        if(!ifprime[i]){
-            for(ll j = i*i; j < maxx; j += i+i){
+void Sieve() {
+    for (ll i = 3; i * i <= maxx; i += 2) {
+        if (!ifprime[i]) {
+            for (ll j = i * i; j < maxx; j += i + i) {
                 ifprime[j] = true;
             }
         }
     }
     primes.pb(2);
-    for(ll i = 3; i < maxx; i+=2) if(!ifprime[i]) primes.pb(i);
+    for (ll i = 3; i < maxx; i += 2) if (!ifprime[i]) primes.pb(i);
 }
 
-bool pcheck(ll n){
-    if(n < 2) return false;
-    if(n == 2) return true;
-    if(!(n&1)) return false;
-    if(!ifprime[n]) return true;
+bool pcheck(ll n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (!(n & 1)) return false;
+    if (!ifprime[n]) return true;
     else return false;
 }
 
-int main(){
+int main() {
     Sieve();
 }
