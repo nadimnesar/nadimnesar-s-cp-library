d9e757b8e2e3ffce0f7169987b0c3fa8f8d2c14e
Nesar Ahmed <nadimnesar.cse@gmail.com>
2023-10-04 12:56:07 +0600

    up

--- Number Theory/Modular.cpp
+++ Number Theory/Modular.cpp
@@ -1,22 +1,22 @@
-ll big_mod(ll B, ll P, ll M){
-    if(P==0) return 1;
-    if(P%2==0){
-        ll p1=(big_mod(B,P/2,M))%M;
-        return (p1*p1)%M;
+ll big_mod(ll B, ll P, ll M) {
+    if (P == 0) return 1;
+    if (P & 1) {
+        ll p1 = B % M;
+        ll p2 = (big_mod(B, P - 1, M)) % M;
+        return (p1 * p2) % M;
     }
-    else{
-        ll p1=B%M;
-        ll p2=(big_mod(B,P-1,M))%M;
-        return (p1*p2)%M;
+    else {
+        ll p1 = (big_mod(B, P / 2, M)) % M;
+        return (p1 * p1) % M;
     }
 }
 
-ll inverse_mod(ll B, ll M){
-    return big_mod(B, M-2, M);
+ll inverse_mod(ll B, ll M) {
+    return big_mod(B, M - 2, M);
 }
 
-ll minus_mod(ll A, ll B, ll M){
-    ll res = (A - B)%M;
-    if(res < 0) res += M;
-    return (res%M);
+ll minus_mod(ll A, ll B, ll M) {
+    ll res = (A - B) % M;
+    if (res < 0) res += M;
+    return (res % M);
 }
