a39441b6f4c590f46c8d9c3c06255320940b48ad
Nesar Ahmed <nadimnesar.cse@gmail.com>
2023-10-14 23:27:42 +0600

    up

--- Miscellaneous/Others/C++ Shortcut.cpp
+++ Miscellaneous/Others/C++ Shortcut.cpp
@@ -1,6 +1,6 @@
 #define llu                 long long unsigned int
 #define ll                  long long int
- 
+
 #define ff                  first
 #define ss                  second
 #define pb                  push_back
@@ -14,7 +14,8 @@
 #define iremove(x,y)        x.erase(x.begin()+y)
 #define dot(x)              fixed<<setprecision(x)
 #define rremove(x, y)       x.erase(remove(x.begin(),x.end(),y),x.end())
-#define alluniq(x, y)       x.resize(distance(x.begin(),unique(x.begin(),x.begin()+y)))
+#define alluniq(x)    		sort(full(x)); x.resize(distance(x.begin(),unique(full(x))));
+
 #define fastIO              ios_base::sync_with_stdio(false);cin.tie(nullptr)
 
 #define EPS                 (1e-7)
@@ -22,10 +23,10 @@
 #define MOD                 (1e9+7)
 #define PI                  (acos(-1.0))
 #define LOG(b, x)           (log(x)/log(b))
- 
+
 #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename T> void __f(const char* X, T &&Y){cerr <<'#'<<X<<": "<<Y<<endl;}
-template <typename T, typename... Z> void __f(const char* N, T&& Y, Z &&...M){cerr<<'#'; }
+template <typename T> void __f(const char* X, T &&Y) {cerr << '#' << X << ": " << Y << endl;}
+template <typename T, typename... Z> void __f(const char* N, T&& Y, Z &&...M) {cerr << '#'; }
 
 #define ll                  long long int
 #define llu                 long long unsigned int
@@ -52,11 +53,16 @@ template <typename T, typename... Z> void __f(const char* N, T&& Y, Z &&...M){ce
 #define log(b, x)           (double)(log(x)/log(b))
 
 untype llu GCD(T a, T b) {if (b == 0) return a; return GCD(b, a % b);}
-untype void vout(vector < T > &v) { for (llu i = 0; i < v.size(); i++) {
-(i + 1 == v.size()) ? (cout << v[i] << endl) : (cout << v[i] << ' '); } }
+untype void vout(vector < T > &v) {
+	for (llu i = 0; i < v.size(); i++) {
+		(i + 1 == v.size()) ? (cout << v[i] << endl) : (cout << v[i] << ' ');
+	}
+}
 untype llu LCM(T a, T b) {a = abs(a); b = abs(b); return (a / GCD(a, b)) * b;}
-untype llu binpow(T n, T p){ if(p == 0) return 1; llu res = binpow(n, p/2);
-if(p%2) return res*res*n; else return res*res; }
+untype llu binpow(T n, T p) {
+	if (p == 0) return 1; llu res = binpow(n, p / 2);
+	if (p % 2) return res * res * n; else return res * res;
+}
 
 ll dx[] = { 0, 0, 1, -1 };
 ll dy[] = { 1, -1, 0, 0 };
@@ -64,20 +70,20 @@ ll dy[] = { 1, -1, 0, 0 };
 ll knightx[8] = { +2, +2, -2, -2, +1, -1, -1, +1 };
 ll knighty[8] = { -1, +1, +1, -1, +2, +2, -2, -2 };
 
-bool valid(ll r, ll c){
-	if(r < 1) return false;
-	if(r > 8) return false;
-	if(c < 1) return false;
-	if(c > 8) return false;
+bool valid(ll r, ll c) {
+	if (r < 1) return false;
+	if (r > 8) return false;
+	if (c < 1) return false;
+	if (c > 8) return false;
 	return true;
 }
 
- #ifdef ONLINE_JUDGE
-        clock_t tStart = clock();
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
+#ifdef ONLINE_JUDGE
+clock_t tStart = clock();
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
 #endif
 
 #ifdef ONLINE_JUDGE
-        fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
 #endif
