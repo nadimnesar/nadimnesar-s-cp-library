848f7f2c080ae9ff9d696b54a36a035d31fab4bd
Nesar Ahmed <nadimnesar.cse@gmail.com>
2023-10-15 12:31:57 +0600

    up

---
+++ String/Hashing/Update Double Hash.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll    long long int
+#define endl  '\n'
+#define ff    first
+#define ss    second
+#define in    insert
+#define pb    push_back
+
+#define full(x) x.begin(),x.end()
+#define dot(x)  fixed<<setprecision(x)
+
+#define gcd(x, y) (ll)(__gcd(x, y))
+#define lcm(x, y) (ll)((x/gcd(x,y))*y)
+#define log(b, x) (double)(log(x)/log(b))
+
+const ll mod = 1e9 + 7;
+const ll maxx = 1e6 + 7;
+const double eps = 1e-7;
+const double pi = acos(-1.0);
+
+void solve() {
+	
+}
+
+int32_t main() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+
+	solve();
+	
+	return 0;
+}
